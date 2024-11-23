3a5a1bbaea3e7aea70d77ae604e93aae1c19b748
yass3rbouh1a <yasser.ali.bouhia@gmail.com>
2024-11-23 15:10:34 +0100

    23/11/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_mobile_app/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      );
+      debugShowCheckedModeBanner: true,
+      home: HomePage()
+    );
   }
 }
