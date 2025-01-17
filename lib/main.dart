dfea3def4da4bc39887913f99a4b03255f0b075f
yass3rbouh1a <yasser.ali.bouhia@gmail.com>
2025-01-17 20:06:53 +0100

    fixing firebase installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_mobile_app/pages/home.dart';
+import 'package:my_first_mobile_app/presentation/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:my_first_mobile_app/firebase_options.dart'; // Import pour DefaultFirebaseOptions
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  initInjection();
+
+  initInjection(); // Assurez-vous que cette fonction initialise les dépendances nécessaires
+
   runApp(const MyApp());
 }
-/* hey */
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-       create: (_) => getIt<CarBloc>()..add(LoadCars()),
+      create: (_) => getIt<CarBloc>()..add(LoadCars()), // Assurez-vous que getIt est bien configuré
       child: MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,
-          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            useMaterial3: true,
+          ),
         ),
-        home: OnboardingPage(),
-     ),
+        home: const OnboardingPage(), // Ajout du const si applicable
+      ),
     );
   }
 }
